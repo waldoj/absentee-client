9e712e94ef28ab3c20037606a6ad369bedd6e1ba
dleichtman <dleichtman@gmail.com>
2015-09-06 14:24:29 -0400

    Update functions.js
    Adds autofill of form fields from GET query parameters.

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -81,6 +81,29 @@
 	/* trigger when page is ready */
 	$(document).ready(function (){
 
+		// Define method for retrieving query params -> assoc array
+        	function getUrlVars()
+        	{
+        		var vars = [], hash;
+        		var hashes = window.location.href.slice(
+        			window.location.href.indexOf('?') + 1).split('&');
+        		for(var i = 0; i < hashes.length; i++)
+        		{
+        			hash = hashes[i].split('=');
+        			vars.push(hash[0]);
+        			vars[hash[0]] = hash[1];
+        		}
+        		return vars;
+        	}
+	
+		// Get query params
+		var queryVars = getUrlVars();
+
+	        // Iterate over query params, fill form field values accordingly
+	        $.each(queryVars, function(key, value) {
+	          $("#"+value).val(queryVars[value]);
+	        });
+
 		// hide our dialog boxes
 		$('#modal-success').modal({ show: false})
 		$('#modal-failure').modal({ show: false})
